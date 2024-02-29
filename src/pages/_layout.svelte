25f5aaf9e13506a656632928f952b5c695a040a1
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2024-02-28 19:07:32 -0500

    vercel analytics

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,5 +1,9 @@
 <script>
   import Footer from './_components/Footer.svelte';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <style>

11072d1d15ac70b90a11e53cbfd59b193ce4b100
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2024-11-03 23:30:20 -0500

    get involved update

--- src/pages/get_involved.svelte
+++ src/pages/get_involved.svelte
@@ -31,8 +31,7 @@
 <div>
   <h2>Join CDJ!</h2>
   <p>We welcome students of all backgrounds and disciplines to come create with us. Whether you've worked with data before or not, anyone is welcome to contribute and publish with us.</p>
-  <p><b>Fall Recruitment information:</b></p>
-  <p>Apply by 9/11 11:59 PM at <a href="https://forms.gle/R8xe8NMqygyMHTWEA">this link</a>.</p>
+  <p><b>We will resume recruitment in Spring 2025.</b></p>
   <h2>Events</h2>
   <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=Y29ybmVsbGRhdGFqb3VybmFsQGdtYWlsLmNvbQ&amp;color=%237CB342&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showDate=1&amp;showNav=1&amp;showTitle=0" height="600" scrolling="no" title="CDJ Event Calendar"></iframe>
 </div>
